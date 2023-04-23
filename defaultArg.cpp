0cfa1cb3756d22fb63fbbdbf8b81a3ee637b750c
Harsh Upadhyay <114221468+harshvns@users.noreply.github.com>
2023-04-24 00:54:49 +0530

    Add files via upload

---
+++ defaultArg.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+// defaultArgs
+void print(int arr[],int n,int start =0){
+    for(int i=start;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+}
+int main() {
+    int arr[5]={1,2,3,4,5};
+    int size=5;
+    print(arr,size);
+    // it take default value of start i.e 0
+    cout<<endl;
+    print(arr,size,2);
+
+    return 0;
+}
